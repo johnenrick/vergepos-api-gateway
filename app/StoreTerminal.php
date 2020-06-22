487516d449dcfcd365ae07df28bbbac37c28867c
johnenrick <johnenrickplenos@gmail.com>
2020-06-22 15:27:31 +0800

    StoreTerminal

--- app/StoreTerminal.php
+++ app/StoreTerminal.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StoreTerminal extends GenericModel
 {
-    protected $validationRuleNotRequired = ['serial_number'];
+    protected $validationRuleNotRequired = ['serial_number', 'accreditation_number', 'machine_identification_number', 'permit_number'];
     public function store(){
         return $this->belongsTo('App\Store');
     }
